0d0b7e867ae1328921d84338e8657e375732ae59
Michiel De Smet <michiel.desmet@starburstdata.com>
2023-06-03 12:32:05 +0200

    Modified to full table nation test

--- core/trino-main/src/test/java/io/trino/server/TestArrowQueryResource.java
+++ core/trino-main/src/test/java/io/trino/server/TestArrowQueryResource.java
@@ -88,7 +88,7 @@ public class TestArrowQueryResource
     public void testNation()
             throws Exception
     {
-        String sql = "SELECT * FROM tpch.tiny.nation order by 1 limit 10";
+        String sql = "SELECT * FROM tpch.tiny.nation order by 1";
         assertArrowResult(sql, ImmutableList.of("""
                 nationkey	name	regionkey	comment
                 0	ALGERIA	0	 haggle. carefully final deposits detect slyly agai
@@ -101,6 +101,21 @@ public class TestArrowQueryResource
                 7	GERMANY	3	l platelets. regular accounts x-ray: unusual, regular acco
                 8	INDIA	2	ss excuses cajole slyly across the packages. deposits print aroun
                 9	INDONESIA	2	 slyly express asymptotes. regular deposits haggle slyly. carefully ironic hockey players sleep blithely. carefull
+                10	IRAN	4	efully alongside of the slyly final dependencies.\s
+                11	IRAQ	4	nic deposits boost atop the quickly final requests? quickly regula
+                12	JAPAN	2	ously. final, express gifts cajole a
+                13	JORDAN	4	ic deposits are blithely about the carefully regular pa
+                14	KENYA	0	 pending excuses haggle furiously deposits. pending, express pinto beans wake fluffily past t
+                15	MOROCCO	0	rns. blithely bold courts among the closely regular packages use furiously bold platelets?
+                16	MOZAMBIQUE	0	s. ironic, unusual asymptotes wake blithely r
+                17	PERU	1	platelets. blithely pending dependencies use fluffily across the even pinto beans. carefully silent accoun
+                18	CHINA	2	c dependencies. furiously express notornis sleep slyly regular accounts. ideas sleep. depos
+                19	ROMANIA	3	ular asymptotes are about the furious multipliers. express dependencies nag above the ironically ironic account
+                20	SAUDI ARABIA	4	ts. silent requests haggle. closely express packages sleep across the blithely
+                21	VIETNAM	2	hely enticingly express accounts. even, final\s
+                22	RUSSIA	3	 requests against the platelets use never according to the quickly regular pint
+                23	UNITED KINGDOM	3	eans boost carefully special requests. accounts are. carefull
+                24	UNITED STATES	1	y final packages. slow foxes cajole quickly. quickly silent platelets breach ironic accounts. unusual pinto be
                 """));
     }
 
@@ -139,7 +154,7 @@ public class TestArrowQueryResource
     public void testCount()
             throws Exception
     {
-        String sql = "SELECT * FROM tpch.tiny.orders";
+        String sql = "SELECT orderkey FROM tpch.tiny.orders";
         List<VectorSchemaRoot> vectorSchemaRoots = getVectorSchemaRoots(sql);
         assertEquals(7500, vectorSchemaRoots.stream().map(VectorSchemaRoot::getRowCount).reduce(0, Integer::sum).intValue());
     }
